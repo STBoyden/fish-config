3519b6d5b2be66e5bbb13649d6b1ed367d17758c
STBoyden <sam@stboyden.com>
2023-06-20 02:41:04 +0100

    Add config function for convenient configuration

--- conf.d/functions.fish
+++ conf.d/functions.fish
@@ -9,6 +9,43 @@ function notes
   end
 end
 
+function config
+  set what $argv[1]
+  set config_dir "$HOME/.config"
+
+  switch $what
+  case wezterm terminal
+    if test -d "$config_dir/wezterm"
+      cd "$config_dir/wezterm"
+      nvim .
+    else
+      echo "Wezterm config directory not found. Ignoring..."
+    end
+  case nvim neovim
+    if test -d "$config_dir/nvim/lua/user"
+      cd "$config_dir/nvim/lua/user"
+      nvim .
+    else
+      echo "Neovim config directory not found. Ignoring..."
+    end
+  case starship prompt
+    if test -f "$config_dir/starship.toml"
+      cd "$config_dir"
+      nvim starship.toml
+    else
+      echo "Starship configuration file not found. Ignoring..."
+    end
+  case fish shell
+    if test -d "$config_dir/fish"
+      cd "$config_dir/fish"
+      nvim .
+    end
+  case '*'
+    echo "Please enter a valid config option."
+    echo "Valid options are: wezterm, terminal, nvim, neovim, starship, prompt, fish, shell"
+  end
+end
+
 function neorg
   set workspace $argv[1]
 
